5301a913992c78d94e64b4c29b89a719ce700fcd
Michael Aquilina <michaelaquilina@gmail.com>
2013-06-10 14:01:00 +0300

    Update explore.sh

--- explore.sh
+++ explore.sh
@@ -2,6 +2,8 @@
 # Rename this file to 'explore' (without .sh) and move it to /usr/bin to allow
 # proper use.
 
+# Works on Ubuntu or other Gnome-based linux distros
+
 if [ "$#" -eq "1" ] 
 then
   nautilus $1
