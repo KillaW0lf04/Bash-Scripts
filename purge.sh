cf455558bfbaa49e1296e34e203d04a846d1ab3f
KillaW0lf04 <michaelaquilina@gmail.com>
2013-06-18 15:31:14 +0200

    Added description to purge script.

--- purge.sh
+++ purge.sh
@@ -1,3 +1,9 @@
+# Purges files of the specified pattern from all folders
+# and subfolders found in the current directory.
+
+# Very useful for removing temporary files such as
+# python pyc files which could be outdated.
+
 if [ $# -eq 0 ]
 then
    echo "You must specify a glob file pattern to purge."
